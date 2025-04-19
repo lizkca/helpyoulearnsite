e3580d5984b1755a95c337e28fbfa09a9170f8eb
lizkca <lienxiong@outlook.com>
2025-04-19 13:27:49 +0800

    fix wsgi.py

--- helpyoulearnsite/wsgi.py
+++ helpyoulearnsite/wsgi.py
@@ -1,4 +1,9 @@
 import os
+import sys
+
+# 添加项目根目录到 Python 路径
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'helpyoulearnsite.settings')
