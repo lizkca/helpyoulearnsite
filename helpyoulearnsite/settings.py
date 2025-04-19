c5e02d981ae535b5dcf041aa95b02f13b7be9f83
lizkca <lienxiong@outlook.com>
2025-04-19 12:49:26 +0800

    deploy render

--- helpyoulearnsite/settings.py
+++ helpyoulearnsite/settings.py
@@ -1,14 +1,39 @@
-# ... 其他设置保持不变 ...
+import os
+import dj_database_url
 
-from django.utils.translation import gettext_lazy as _
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = 'RENDER' not in os.environ
 
+ALLOWED_HOSTS = []
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+
+# Database
+# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
+}
+
+# Static files (CSS, JavaScript, Images)
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# 添加 whitenoise 中间件
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # 添加这行
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',  # 添加这行
     'django.middleware.common.CommonMiddleware',
     # ... 其他中间件 ...
 ]
+
+# whitenoise 设置
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LANGUAGES = [
     ('en', _('English')),
